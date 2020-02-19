043e74384f48aa151bbc47fe13b0c9b5d41dae97
Luca <luca-marconi@hotmail.com>
2020-02-19 20:54:23 +0100

    esercizio dadi

--- js/main.js
+++ js/main.js
@@ -2,15 +2,40 @@
 // chiedi il nome all utente
 // se il nome risulta in lista -> accedi
 // altrimenti -> accesso negato
-
+//
 var listaAccessi = ['Luca', 'Mario', 'Giovanni', 'Andrea', 'Marta'];
 console.log(listaAccessi);
 var nome = prompt('Come ti chiami?');
 
 for (var i = 0; i < listaAccessi.length; i++) {
+    // console.log(listaAccessi[i]);
     if (listaAccessi[i] == nome) {
-        console.log('puoi entrare');
-    } else {
-        console.log('non puoi entrare');
+        var nomeInLista = true;
     }
 }
+
+if (nomeInLista == true) {
+    console.log('Può entrare');
+} else {
+    console.log('Non può entrare');
+}
+
+
+// ----------------esercizio dadi----------------------
+
+
+var player1 = Math.random() * 10;
+var tiroUno = player1.toFixed(0);
+console.log(tiroUno);
+
+var player2 = Math.random() * 10;
+var tiroDue = player2.toFixed(0);
+console.log(tiroDue);
+
+if (tiroUno > tiroDue) {
+    console.log('Player 1 ha vinto');
+} else if
+    (tiroUno < tiroDue) {
+        console.log('Player 2 ha vinto');
+} else
+    console.log('Pareggio');
